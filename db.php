7b1da794f54c7ce96ae8ce82267640af2aed99d8
gurux13 <gurux13@gmail.com>
2020-10-25 19:12:15 +0000

    getenv for env vars

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$link = mysqli_connect("localhost", "passgen", $_ENV['MYSQL_PASSWORD']) or die (mysql_error());
+$link = mysqli_connect("localhost", "passgen", getenv('MYSQL_PASSWORD')) or die (mysql_error());
 mysqli_select_db($link, "passgen") or die(mysql_error());
 ?>
