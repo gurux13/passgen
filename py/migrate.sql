66f44ff41a31e15ec51276482099f4b31c9dabdb
gurux13 <gurux13@gmail.com>
2024-04-23 23:28:09 +0100

    Migration script fix

--- py/migrate.sql
+++ py/migrate.sql
@@ -14,7 +14,7 @@ INSERT INTO `passgen_py`.`user`
 VALUES
 ('1', 'gurux13', '2022-06-05 08:30:36', 'pbkdf2:sha256:260000$YZjC0YcpgNOpYHry$fb5dc2b0d236275a57088a805cccd58f859ef2f6eb12b635a102234192961bb3', NULL, NULL),
 ('2', 'anna2604', '2022-06-05 11:35:07', 'pbkdf2:sha256:260000$8dBFM4NDHA3mtxKM$e2d95d5b55949e2a60b00e9004292634e021d6632982eea89376de6c9293d780', NULL, NULL);
-/*
+
 
 INSERT INTO `passgen_py`.`resource`
 (`login_id`,
@@ -46,14 +46,14 @@ WHERE `variants`.`login_id` IN (
 INSERT INTO `passgen_py`.`resource_account`
 (
 `resource_id`,
-`pass_part`,
+--`pass_part`,
 `revision`)
 SELECT
     resource.id,
-    variants.resource,
+--    variants.resource,
 
     `variants`.`revision`
 FROM `passgen`.`variants`, passgen_py.resource
 where variants.resource = resource.name and variants.login_id = resource.login_id;
-*/
+
 COMMIT;
